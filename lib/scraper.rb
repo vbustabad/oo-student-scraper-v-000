abeb67df19fb9dc158470ba066b3bf2da31188b1
Veronica Bustabad <vbustabad@gmail.com>
2018-05-06 02:53:40 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -32,7 +32,7 @@ class Scraper
         student[:blog] = link
       end
     end
-  
+
     student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
     student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
 
